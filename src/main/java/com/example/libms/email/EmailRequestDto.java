37922c191ce63e88be22f37d03d8f2cad19fc692
Adrien Hammoud <Andrien.Hammoud@areeba.com>
2026-06-04 16:58:55 +0300

    added the code for the ms2 integration

--- src/main/java/com/example/libms/email/EmailRequestDto.java
+++ src/main/java/com/example/libms/email/EmailRequestDto.java
@@ -1,4 +1,13 @@
 package com.example.libms.email;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class EmailRequestDto {
+    private String email;
+    private String message;
 }
--- src/main/java/com/example/libms/email/EmailServiceClient.java
+++ src/main/java/com/example/libms/email/EmailServiceClient.java
@@ -1,4 +1,12 @@
 package com.example.libms.email;
 
-public class EmailService {
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "email-service", url = "${services.email.url}")
+public interface EmailServiceClient {
+
+    @PostMapping("/email/send-email")
+    void sendEmail(@RequestBody EmailRequestDto request);
 }
